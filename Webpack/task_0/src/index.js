b793e9bacbc6cf4b549639ebae36cb096ebc6c4e
soO <444deph12@gmail.com>
2024-01-07 07:30:02 +0000

    TypeScript/Webpack task 0

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+  const paragraphs = [
+    "Holberton Dashboard",
+    "Dashboard data for the students",
+    "Copyright - Holberton School",
+  ];
+
+  paragraphs.forEach((text) => {
+    $("body").append(`<p>${text}</p>`);
+  });
+});
